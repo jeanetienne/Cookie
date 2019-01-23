76bb6d8b0f061742e7a7c606520d5d546d82bc6c
Jean-Étienne <git@jeanetienne.net>
2019-01-24 10:40:35 +1100

    Updating copyright notice

--- Cookie/main.swift
+++ Cookie/main.swift
@@ -1,9 +1,6 @@
 //
-//  main.swift
 //  Cookie
-//
-//  Created by Jean-Étienne on 3/6/17.
-//  Copyright © 2017 Jean-Étienne. All rights reserved.
+//  Copyright © 2019 Jean-Étienne. All rights reserved.
 //
 
 import Foundation
